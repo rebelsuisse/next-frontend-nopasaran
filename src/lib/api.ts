100d3b13a9197cbf3459804b8bd77c56239070d6
Rebel Suisse <rebel.suisse@gmail.com>
2025-11-12 12:18:41 +0100

    Minor modif

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,21 +1,26 @@
- import { Incident, StrapiApiResponse } from "@/types";
+import { Incident, StrapiApiResponse } from "@/types";
 
- export async function getIncidents(): Promise<StrapiApiResponse<Incident[]>> {
-   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/the-wall-of-shames?populate=*`);
+export async function getIncidents(): Promise<StrapiApiResponse<Incident[]>> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/the-wall-of-shames?populate=*`);
 
-   if (!res.ok) {
-     throw new Error('Failed to fetch incidents');
-   }
+    if (!res.ok) {
+        // On récupère le corps de la réponse d'erreur
+        const errorDetails = await res.json(); 
+        console.error("Strapi API Error:", errorDetails.error);
 
-   return res.json();
- }
+        // On peut même construire un message d'erreur plus détaillé
+        throw new Error(`Failed to fetch incidents: ${res.status} ${res.statusText} - ${errorDetails.error.message}`);
+    }
 
- export async function getIncidentBySlug(slug: string): Promise<StrapiApiResponse<Incident[]>> {
-    // L'API de Strapi permet de filtrer par n'importe quel champ
-   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/the-wall-of-shames?filters[slug][$eq]=${slug}&populate=*`);
+    return res.json();
+}
 
-   if (!res.ok) {
-     throw new Error(`Failed to fetch incident: ${slug}`);
-   }
-   return res.json();
- }
+export async function getIncidentBySlug(slug: string): Promise<StrapiApiResponse<Incident[]>> {
+// L'API de Strapi permet de filtrer par n'importe quel champ
+    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/the-wall-of-shames?filters[slug][$eq]=${slug}&populate=*`);
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch incident: ${slug}`);
+    }
+    return res.json();
+}
