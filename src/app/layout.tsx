6c590ebe9962a1be931e694573c5b3737e2e5912
Rebel Suisse <rebel.suisse@gmail.com>
2025-11-14 15:58:41 +0100

    Improvements with translations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nopasaran.ch - The Wall of Shame",
+  title: "No pasaran - The Wall of Shame",
   description: "Un registre des incidents liés à l'extrême droite en Suisse.",
 };
 
