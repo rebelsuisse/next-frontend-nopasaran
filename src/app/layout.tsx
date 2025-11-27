9d15fa18d1528e3d112d2d2d24d2ddc322bd1333
Rebel Suisse <rebel.suisse@gmail.com>
2025-11-27 18:50:29 +0100

    CHange logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,15 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://www.nopasaran.ch'),
   title: "No pasaran - The Wall of Shame",
   description: "Un projet citoyen pour la transparence politique en Suisse.",
+
+  icons: {
+    // L'icône principale (haute résolution pour Google)
+    icon: '/icon.png', 
+    // L'icône pour raccourci (le vieux favicon.ico si vous l'avez)
+    shortcut: '/favicon.ico',
+    // L'icône pour iPhone/iPad (Apple Touch Icon)
+    apple: '/icon.png', // Vous pouvez utiliser la même si elle est carrée
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
